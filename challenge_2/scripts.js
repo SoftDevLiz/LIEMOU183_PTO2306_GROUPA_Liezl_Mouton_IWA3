0ac3e69006dc55320a7bff3c5ce6d68cc16f3d62
Liezl Mouton <liz.mouton1@gmail.com>
2023-09-16 16:29:12 +0200

    Imported necessary variables + changed console.log approach
    It was being fetched from objects in console.log statement but I changed it so that it was being fetched as the variables themselves

--- challenge_2/scripts.js
+++ challenge_2/scripts.js
@@ -1,3 +1,5 @@
-import { role } from "./nwabisa.js";
+import { role as nwabisaRole } from "./nwabisa.js";
+import { role as johannesRole } from "./johannes.js";
+import { role as alexRole } from "./alex.js";
 
-console.log("Roles:", nwabisa.role, johannes.role, alex.role);
+console.log("Roles:", nwabisaRole, ",", johannesRole, ",", alexRole);
